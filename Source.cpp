28f37a4fe9f53359730424e9aff08727390abc3b
Ilya Barsukov <amenohitottsunokami@yandex.ru>
2024-11-18 00:25:45 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,16 @@
 #include "./core/Evaluator.h"
 
-#include <iostream>
-
 int main() {
     calc::VariableMap<double> variables = { {"x", 27}, {"y", 3} };
     calc::Evaluator<double> eval;
 
-    try {
-        auto result = eval.evaluate("5 + sqrt((x + y) / 3 + 2 * y, 2) + 5", variables);
-        std::cout << "Result: " << result << std::endl;
-    }
-    catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
+    auto result = eval.evaluate("5 + sqrt((x + y) / 3 + 2 * y, 2) + 5", variables);
+
+    std::cout << "Result: " << (result
+        ? std::to_string(*result)
+        : "no value.") << std::endl;
+
+
 
     return 0;
 }
