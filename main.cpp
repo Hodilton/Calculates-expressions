2501ce11a872253e632e2b3817a9f4c766668f8e
Ilya Barsukov <amenohitottsunokami@yandex.ru>
2024-12-08 11:38:39 +0700

    новый пример

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@ int main() {
     calc::types::VariableMap<double> variables = { {"x", 27}, {"y", 3} };
     calc::Calculator calc;
 
-    auto result = calc.evaluate<double>("5 + sqrt((x + y) / 3 + 2 * y, 2) + 5", variables);
+    //auto result = calc.evaluate<double>("5 + sqrt((x + y) / 3 + 2 * y, 2) + 5 + pi", variables);
+    auto result = calc.evaluate<double>("-(-x + 5) + 5 + sqrt2(x + 9, 2)", variables);
 
     std::cout << "Result: " << (result ? std::to_string(*result) : "no value.") << std::endl;
 
